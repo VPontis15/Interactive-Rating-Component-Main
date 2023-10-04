87071855c2f8d4612ff603d67a1fb119bf6d23c6
VPontis15 <94546957+VPontis15@users.noreply.github.com>
2023-10-04 18:17:42 +0300

    finish js

--- script.js
+++ script.js
@@ -2,8 +2,14 @@
 
 const submitBtn = document.getElementById("submit");
 const card = document.querySelector(".card");
+const resultCard = document.querySelector(".result");
+const resultContainer = document.querySelector(".result-container");
 
 submitBtn.addEventListener("click", () => {
   let value = document.querySelector('input[name="btn"]:checked').value;
   card.classList.add("submitted");
+  resultCard.classList.remove("hidden");
+  let btns = document.querySelectorAll(".btn");
+  let result = ` <p class="result-text">You selected ${value} out of ${btns.length}</p>`;
+  resultContainer.insertAdjacentHTML("afterbegin", result);
 });
